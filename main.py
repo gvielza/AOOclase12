0ee731201c43e18ad7e1f95d7f7b1efe77a748c9
gvielza <voltagnelly@gmail.com>
2023-10-02 14:42:54 +0000

    inicio del proyecto

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hola desde Replit")
+#-Crear una clase Automóvil con tenga por defecto atributo ruedas inicializado en 4, con un #constructor con los parámetros de color, marca, aceleracion y velocidad
+
+class Automovil:
+  ruedas=4
+  def __init__(self,color,marca,aceleracion):
+    self.color=color
+    self.marca=marca
+    self.aceleracion=aceleracion
+    self.velocidad=0
+
